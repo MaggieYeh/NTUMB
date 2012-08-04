11d62ab4b24b2d3b5395c50a52f9810a79d94838
Jack Chen <lalala.jack@gmail.com>
2012-08-04 22:24:39 +0800

    first commit.(Active Admin not installed yet)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NTUMB::Application.config.secret_token = '39068dea71be5bf04d7e0d70671d7ec86cd3c869e4d4536b252575e3f1df2e1ecb6cb4a390eba5924781bfb8ac6034d5f532c9543037ce562aa7ae53e814dd8b'
