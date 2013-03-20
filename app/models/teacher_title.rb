16a21c61602310b7abdfa2373127349fce95323b
Jack Chen <b98207070@ntu.edu.tw>
2013-03-20 11:10:58 +0800

    Fix Honory professor to Emeritus

--- app/models/teacher_title.rb
+++ app/models/teacher_title.rb
@@ -2,7 +2,7 @@
 class TeacherTitle < ActiveRecord::Base
   #TITLES_en = %w[chairperson distinguished_chair visiting honory proffesor associate assistant adjunct adjunct_associate adjunct_assistant professional_practice_teacher]
   TITLES_en = ["Chair Person", "Distinguished Chair Professor", "Visiting Professor", 
-               "Honory Professor", "Professor", "Associate Professor", "Assistant Professor",
+               "Emeritus Professor", "Professor", "Associate Professor", "Assistant Professor",
                "Adjunct Professor", "Adjunct Associate Professor", "Adjunct Assisitant Professor",
                "Professional Practice Teacher", "Adjunct Instructor"]
   TITLES_zh = %w[系主任 特聘講座教授 客座教授 名譽教授 專任教授 專任副教授 專任助理教授 兼任教授 兼任副教授 兼任助理教授 專業實務教師 兼任講師]
